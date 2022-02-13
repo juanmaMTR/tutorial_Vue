75de5ac7b42f54f91aa2d5569999d951d7949065
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-02-13 20:45:25 +0100

    Tutorial Vue V.3

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const app=Vue.createApp({
     data(){ //Datos qe recibe la aplicaión de Vue
         return{
             producto: 'Calcetines',
+            marca: 'Marca Vue',
             descripcion: 'Hecho con la mejor lana del mundo',
             imagen: './assets/images/socks_blue.jpg',
             url: 'https://es.wikipedia.org/wiki/Calcet%C3%ADn',
@@ -14,10 +15,22 @@ const app=Vue.createApp({
             oferta: true,
             detalles: ['50% algodón', '30% lana', '20% otros materiales'],
             colores: [
-                { id: 'A020', color: 'Green', imagen: './assets/images/socks_green.jpg' },
-                { id: 'B022', color: 'Blue', imagen: './assets/images/socks_blue.jpg' }
+                { id: 'A020', color: 'Green', imagen: './assets/images/socks_green.jpg', cantidad:20 },
+                { id: 'B022', color: 'Blue', imagen: './assets/images/socks_blue.jpg', cantidad:0 }
             ],
             carro: 0,
+            seleccionarVariante:0,
+        }
+    },
+    computed:{
+        titulo(){
+            return this.marca+' '+this.producto
+        },
+        imagen(){
+            return this.colores[this.seleccionarVariante].imagen
+        },
+        cantidad(){
+            return this.colores[this.seleccionarVariante].cantidad
         }
     },
     methods:{
@@ -31,6 +44,9 @@ const app=Vue.createApp({
         },
         actualizarImagen(urlImagen){
             this.imagen= urlImagen
+        },
+        actualizarVariante(indice){
+            this.seleccionarVariante=indice
         }
     }
 })
